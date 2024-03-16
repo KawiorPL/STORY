dedcbb2e408648cb4278690c54083c663f23f753
Chris kawka <chriskawka@gmail.com>
2024-03-16 16:36:53 +0100

    commit

--- GIT-test.py
+++ GIT-test.py
@@ -20,5 +20,13 @@ for i in range(10):
     list.append(x)
     print(x)
 
+z=maxFun(list)
 print(f"list {list}")
-print(f"max is: {maxFun(list)}")
+print(f"max is: {z}")
+
+y=random.randint(75,100)
+
+if y - z>0:
+    print(f"You win = Your number {y} and my number {z}")
+else:
+    print(f"I win Your number {y} and my number {z}")
